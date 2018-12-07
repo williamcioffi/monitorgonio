baa905efe90f9e076e700a8d7ab4664229c33efa
williamcioffi <williamcioffi@gmail.com>
2018-12-07 09:09:47 -0500

    added simulate_gonio

---
+++ R/pttkey.R
@@ -0,0 +1,9 @@
+#' an example pttkey
+#'
+#' a tiny example pttkey which also works with the simulated goniometer output see\code{\link[monitorgonio]{simulate_gonio}}.
+#' @docType data
+#' @usage data(pttkey)
+#' @format a data.frame
+#' @keywords datasets
+"pttkey"
+
